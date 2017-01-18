8b64aa2e4bfe012a8c667a8116ddba2a05e027dd
wangjun <928639375@qq.com>
2017-01-18 16:46:24 +0800

    commit

--- src/main/java/com/wangzi/ApplicationStart.java
+++ src/main/java/com/wangzi/ApplicationStart.java
@@ -3,8 +3,10 @@ package com.wangzi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Repository;
 
 @EnableAutoConfiguration
 @Configuration
@@ -12,6 +14,10 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 public class ApplicationStart {
 	public static void main(String[] args) {
-		SpringApplication.run(ApplicationStart.class, args);
+		ApplicationContext ctx = SpringApplication.run(ApplicationStart.class, args);
+		String[] beanNames = ctx.getBeanNamesForAnnotation(Repository.class);
+		for(String bn:beanNames){
+			System.out.println(bn);
+		}
 	}
 }
--- src/main/java/com/wangzi/controller/UserController.java
+++ src/main/java/com/wangzi/controller/UserController.java
@@ -3,12 +3,12 @@ package com.wangzi.controller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.wangzi.service.IUserService;
 
 
-@Controller
+@RestController
 public class UserController {
 	private Logger logger = LogManager.getLogger(UserController.class);
 	@Autowired
