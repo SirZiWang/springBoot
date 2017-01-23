333828feacf652de3468162ead498a933ac93af5
wangjun <928639375@qq.com>
2017-01-23 17:11:18 +0800

    定时任务

---
+++ src/main/java/com/wangzi/task/ScheduledTasks.java
@@ -0,0 +1,17 @@
+package com.wangzi.task;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configurable
+@EnableScheduling
+public class ScheduledTasks {
+ 
+	@Scheduled(cron="0 */3 *  * * * ")
+	public void scheduledTest(String str){
+		
+	}
+}
